c94589e2865b303cdf14e3142912cee8bf869b3b
JBarnett775 <joelbarnett2000@gmai.com>
2026-01-26 18:47:13 +0000

    update bucket name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraform-example-bucket" {
-  bucket = "terraform-example-bucket"
+resource "aws_s3_bucket" "P1-bucket" {
+  bucket = "P1-bucket"
 }
 
