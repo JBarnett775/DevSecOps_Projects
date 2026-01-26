4cba016d3b8c7063e97cff9e8ee4170bde0b92f6
JBarnett775 <joelbarnett2000@gmai.com>
2026-01-26 19:43:57 +0000

    bucket2

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "p1-bucket-2-2026" {
-  bucket = "p1-bucket-2-2026"
+resource "aws_s3_bucket" "p1-bucket-2026" {
+  bucket = "p1-bucket-2026"
 }
+
+resource "aws_s3_bucket" "p2-bucket-2026" {
+  bucket = "p2-bucket-2026"
+}
+
 terraform {
   backend "s3" {
-    bucket = "p1-bucket-2-2026"
+    bucket = "p1-bucket-2026"
     key    = "global/s3/terraform.tfstate"
     region = "eu-west-2"
   }
