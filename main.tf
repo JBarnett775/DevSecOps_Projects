f2771331507d4babc16c211059f197d079d83cc0
JBarnett775 <joelbarnett2000@gmai.com>
2026-01-26 19:11:06 +0000

    added backend

--- main.tf
+++ main.tf
@@ -4,4 +4,11 @@ provider "aws" {
 resource "aws_s3_bucket" "p1-bucket-2026" {
   bucket = "p1-bucket-2026"
 }
+terraform {
+  backend "s3" {
+    bucket = "p1-bucket-2026"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
 
