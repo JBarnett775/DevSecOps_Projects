9954c7d5308daf9d232b3a1b08eecafa9a680048
JBarnett775 <joelbarnett2000@gmai.com>
2026-01-26 18:47:42 +0000

    p

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "P1-bucket" {
-  bucket = "P1-bucket"
+resource "aws_s3_bucket" "p1-bucket" {
+  bucket = "p1-bucket"
 }
 
